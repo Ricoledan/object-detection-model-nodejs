c5b99728ce38159d836c1f1fe7cd0884e749c505
Ricardo Ledan <ricoledan@gmail.com>
2021-03-07 23:51:59 -0500

    add logic to run inference on image file and process image into tensor

--- run-tfjs-model.js
+++ run-tfjs-model.js
@@ -1,5 +1,6 @@
 const tf = require('@tensorflow/tfjs-node');
 const fs = require('fs');
+const path = require('path');
 const modelUrl =
   'https://tfhub.dev/tensorflow/tfjs-model/ssdlite_mobilenet_v2/1/default/1';
 let model;
@@ -12,17 +13,25 @@ const loadModel = async function () {
   return model;
 };
 
-// run
-loadModel().then((model) => {
-  console.log(model);
-});
-
 // convert image to Tensor
-const processInput = function () {
-  console.log(`preprocessing image ${imagePath}`);
-  const image = fs.readFileSync(imagePath);
-  const buf = Buffer.from(image);
-  const Uint8Array = new Uint8Array(buf);
+const processInput = function (imgName) {
+  console.log(`preprocessing image ${imgName}`);
+  const img = fs.readFileSync(path.join('assets', imgName));
+  const buf = Buffer.from(img);
+  const uint8Array = new Uint8Array(buf);
 
-  return tf.node.decodeImage(Uint8Array, 3).expandDims();
+  return tf.node.decodeImage(uint8Array, 3).expandDims();
 };
+
+// run
+if (process.argv.length < 3) {
+  console.log('please supply an image to the process');
+  console.log('node run-tfjs-model.js image.jpg');
+} else {
+  let imgName = process.argv[2];
+
+  loadModel().then((model) => {
+    const inputTensor = processInput(imgName);
+    inputTensor.print();
+  });
+}
